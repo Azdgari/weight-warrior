3ba2f928df90f85d1bfef02a681b00766c8d9295
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-11-14 21:56:14 +0700

    Create swipeable component with gesture handler

--- app/(tabs)/stats.js
+++ app/(tabs)/stats.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, FlatList } from 'react-native';
 import styles from '../assets/styles';
 import NewEntryModal from '../(modals)/newEntryModal';
 import StackLayout from '../_layout';
 import { AppStateContext } from '../appStateContext';
 import { db } from '../firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Stats = () => {
   const { sharedState, setSharedState } = useContext(AppStateContext);
@@ -25,10 +27,11 @@ const Stats = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scroll}>
-        {sharedState.map((item) => (
-          <View key={item.id} style={styles.exerciseWrapper}>
+    <GestureHandlerRootView style={styles.container}>
+      <FlatList
+        data={sharedState}
+        renderItem={({ item }) => (
+          <View style={styles.exerciseWrapper}>
             <Text style={styles.exercise}>{item.exerciseName}</Text>
             <Text style={styles.weight}>{item.exerciseWeight} KG</Text>
             <View style={styles.repsContainer}>
@@ -36,9 +39,10 @@ const Stats = () => {
               <Text style={styles.sets}>Sets: {item.exerciseSets}</Text>
             </View>
           </View>
-        ))}
-      </ScrollView>
-    </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </GestureHandlerRootView>
   );
 };
 
