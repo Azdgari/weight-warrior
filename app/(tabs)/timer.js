a279907c1f3d84f23ab6ee13eb18c64f7196ef4a
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-11-22 16:45:25 +0700

    Modify props for clock

--- app/(tabs)/timer.js
+++ app/(tabs)/timer.js
@@ -3,6 +3,7 @@ import { Text, View, Button, Pressable } from 'react-native';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import styles from '../assets/styles';
 import { Feather } from '@expo/vector-icons';
+import colors from '../assets/colors';
 
 export default function App() {
   const [isPlaying, setIsPlaying] = React.useState(true);
@@ -10,22 +11,26 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.timerContainer}>
-        <CountdownCircleTimer
-          isPlaying={isPlaying}
-          duration={60}
-          colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-          colorsTime={[10, 6, 3, 0]}
-          onComplete={() => ({ shouldRepeat: true, delay: 2 })}
-          onPress={() => setIsPlaying((prev) => !prev)}
-        >
-          {({ remainingTime, color }) => (
-            <Text style={{ color, fontSize: 40 }}>{remainingTime}</Text>
-          )}
-        </CountdownCircleTimer>
-        <Button
-          title="Toggle Playing"
-          onPress={() => setIsPlaying((prev) => !prev)}
-        />
+        <Pressable onPress={() => setIsPlaying((prev) => !prev)}>
+          <CountdownCircleTimer
+            isPlaying={isPlaying}
+            duration={60}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[10, 6, 3, 0]}
+            onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+            size={250}
+            strokeWidth={12}
+            // trailStrokeWidth={16}
+            trailColor={colors.primary}
+            strokeLinecap="round"
+          >
+            {({ remainingTime, color }) => (
+              <Text style={{ color, fontSize: 40, fontWeight: 'bold' }}>
+                {remainingTime}
+              </Text>
+            )}
+          </CountdownCircleTimer>
+        </Pressable>
       </View>
       <View>
         <Pressable>
